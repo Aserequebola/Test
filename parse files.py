11c3b688cd1da766b64a102d1348cc530978c60a
eloy <eloy.oliveros@undostres.com.mx>
2022-06-14 09:55:24 -0400

    anuario salud

--- parse files.py
+++ parse files.py
@@ -3,7 +3,7 @@ import os
 import unicodedata
 import json 
 
-path = "./files"
+path = "./files_salud"
 dir_list = os.listdir(path)
 
 lista = {}
@@ -11,9 +11,9 @@ lista['files'] = []
 for elem in dir_list:
     contenido = {}
     contenido['name'] = elem
-    contenido['dir'] = './files/'+elem
+    contenido['dir'] = './files_salud/'+elem
     lista['files'].append(contenido)
 
 # jsLista = json.dumps(lista, ).encode('utf8')
-with open("files.json", "w", encoding='utf8') as outfile:
+with open("files_salud.json", "w", encoding='utf8') as outfile:
     json.dump(lista, outfile, ensure_ascii=False)
